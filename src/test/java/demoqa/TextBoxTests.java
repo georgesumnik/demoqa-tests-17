5b5f62f9c88677c2319b946cb3e340fbdfb8aa1a
George Sumnik <georgesumnik@gmail.com>
2023-03-02 07:26:35 -0500

    updated TextBoxTests.java

--- src/test/java/demoqa/TextBoxTests.java
+++ src/test/java/demoqa/TextBoxTests.java
@@ -45,4 +45,43 @@ public class TextBoxTests {
         $("#output #permanentAddress").shouldHave(text(permanentAddress));
 
     }
+
+    @Test
+    void fullFormTest2() {
+        String firstName = "Geo";
+        String lastName = "Sum";
+        String userEmail = "geosum@gmail.com";
+        String userNumber = "3538274936";
+        String userDob = "11.22.2000";
+        String subjects = "New User";
+        String currentAddress = "1748 Ocean View apt 88";
+        String permanentAddress = "1750 Ocean View apt 90";
+
+        open("/automation-practice-form");
+        $(".main-header").shouldHave(text("Practice Form"));
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $("#gender-radio-1").click();
+        $("#userNumber").setValue(userNumber);
+        $("#dateOfBirthInput").setValue(userDob);
+        $("#subjectsInput").setValue(subjects);
+        $("#hobbies-checkbox-1").click();
+
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+
+        //fixing issue with an add overlaying submit button
+        $("#submit").scrollTo();
+
+        $("#submit").click();
+
+        $("#output").shouldBe(visible);
+        $("#output").$("#name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+
+    }
 }
