63208cd305b92876c12c2fd05cc6c31e1118a39b
George Sumnik <georgesumnik@gmail.com>
2023-08-09 21:49:46 -0400

    updated build.gradle updated TextBoxTests.java

--- src/test/java/demoqa/TextBoxTests.java
+++ src/test/java/demoqa/TextBoxTests.java
@@ -1,23 +1,28 @@
 package demoqa;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
 
     @BeforeAll
     static void beforeAll (){
 
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1024x768";
         Configuration.baseUrl = "https://demoqa.com";
     }
 
+    @AfterAll
+    static void afterAll (){
+        Configuration.holdBrowserOpen = true;
+    }
     @Test
     void fullFormTest() {
         String userName = "Geo Sum";
@@ -50,6 +55,7 @@ public class TextBoxTests {
     void fullFormTest2() {
         String firstName = "Geo";
         String lastName = "Sum";
+        String userName = "Geo Sum";
         String userEmail = "geosum@gmail.com";
         String userNumber = "3538274936";
         String userDob = "11.22.2000";
@@ -63,25 +69,44 @@ public class TextBoxTests {
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
-        $("#gender-radio-1").click();
+
+//        $("#gender-radio-1").click(); //wrong
+//        $("#gender-radio-1").parent().click(); //good
+//        $(byText("Male")).click(); // not so good
+        $("#genterWrapper").$(byText("Male")).click(); // best
+//        $("label[for=gender-radio-1]").$(byText("Male")).click(); // good
+
         $("#userNumber").setValue(userNumber);
-        $("#dateOfBirthInput").setValue(userDob);
+        $("#dateOfBirthInput").setValue(userDob).pressEnter();
+        //$(".main-header").click();//clicking off to get rit of the calendar
         $("#subjectsInput").setValue(subjects);
-        $("#hobbies-checkbox-1").click();
+        $("[for=hobbies-checkbox-1]").scrollTo().click();
+        //$("[for=hobbies-checkbox-1]").click();
 
         $("#currentAddress").setValue(currentAddress);
-        $("#permanentAddress").setValue(permanentAddress);
+
+        $("#submit").pressEnter();
+
+        //actions().sendKeys("TAB");
+        //actions().sendKeys("TAB");
+        //actions().sendKeys("ENTER");
+        //$("#permanentAddress").setValue(permanentAddress);
 
         //fixing issue with an add overlaying submit button
-        $("#submit").scrollTo();
+        //$("#submit").scrollTo();
 
-        $("#submit").click();
+        //$("#submit").click();
 
-        $("#output").shouldBe(visible);
-        $("#output").$("#name").shouldHave(text(userName));
-        $("#output #email").shouldHave(text(userEmail));
-        $("#output #currentAddress").shouldHave(text(currentAddress));
-        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+
+        $("#closeLargeModal").pressEnter();
+
+        //$("#output").shouldBe(visible);
+        //$("#output").$("#name").shouldHave(text(userName));
+        //$("#output #email").shouldHave(text(userEmail));
+        //$("#output #currentAddress").shouldHave(text(currentAddress));
+        //$("#output #permanentAddress").shouldHave(text(permanentAddress));
+
+
 
     }
 }
