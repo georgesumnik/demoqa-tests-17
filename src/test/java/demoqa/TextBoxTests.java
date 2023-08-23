0c48d62973cd5ee1f7bb0b0108bef854d3b8e05e
George Sumnik <georgesumnik@gmail.com>
2023-08-23 03:09:33 -0400

    updated AutomationPracticeForm.java added AutomationPracticeFormWithComments.java

--- src/test/java/demoqa/TextBoxTests.java
+++ src/test/java/demoqa/TextBoxTests.java
@@ -9,6 +9,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class TextBoxTests {
 
@@ -52,7 +53,7 @@ public class TextBoxTests {
     }
 
     @Test
-    void fullFormTest2() {
+    void fullFormTest2() throws InterruptedException {
         String firstName = "Geo";
         String lastName = "Sum";
         String userName = "Geo Sum";
@@ -67,25 +68,41 @@ public class TextBoxTests {
         $(".main-header").shouldHave(text("Practice Form"));
 
         $("#firstName").setValue(firstName);
+        sleep(1000);
         $("#lastName").setValue(lastName);
+        sleep(1000);
         $("#userEmail").setValue(userEmail);
+        sleep(1000);
 
 //        $("#gender-radio-1").click(); //wrong
 //        $("#gender-radio-1").parent().click(); //good
 //        $(byText("Male")).click(); // not so good
         $("#genterWrapper").$(byText("Male")).click(); // best
+        sleep(1000);
 //        $("label[for=gender-radio-1]").$(byText("Male")).click(); // good
 
         $("#userNumber").setValue(userNumber);
+        sleep(1000);
+        $("#dateOfBirthInput").clear();
+        sleep(1000);
         $("#dateOfBirthInput").setValue(userDob).pressEnter();
+        sleep(1000);
         //$(".main-header").click();//clicking off to get rit of the calendar
         $("#subjectsInput").setValue(subjects);
+        sleep(1000);
         $("[for=hobbies-checkbox-1]").scrollTo().click();
+        sleep(1000);
         //$("[for=hobbies-checkbox-1]").click();
 
         $("#currentAddress").setValue(currentAddress);
+        sleep(1000);
 
         $("#submit").pressEnter();
+        //sleep(3000);
+
+        //executeJavaScript("$('#fixedban').remove()");
+        //executeJavaScript("$('footer').remove()");
+        //sleep(3000);
 
         //actions().sendKeys("TAB");
         //actions().sendKeys("TAB");
